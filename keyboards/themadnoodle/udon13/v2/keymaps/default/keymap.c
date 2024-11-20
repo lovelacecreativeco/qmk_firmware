42f8d4c72ee3bbe169121565009abc0ba6185a91
lovelacecreativeco <info@lovelacecreative.co>
2024-11-20 13:43:16 -0800

    MIDI2LR + MACROS UPDATE

--- keyboards/themadnoodle/udon13/v2/keymaps/default/keymap.c
+++ keyboards/themadnoodle/udon13/v2/keymaps/default/keymap.c
@@ -22,11 +22,54 @@ extern MidiDevice midi_device;
 
 // Define a single layer
 enum layers {
-    _LTRM // Single test layer
+    _CULL,
+    _LTRM,
+    _PRST,
+    _SHTCT
 };
-
 // Define custom keycodes
-enum midi_cc_keycodes_LTRM { MIDI_CC1 = SAFE_RANGE, MIDI_CC2, MIDI_CC3, MIDI_CC4, MIDI_CC5, MIDI_CC6, MIDI_CC7, MIDI_CC8, MIDI_CC9, MIDI_CC10, MIDI_CC11, MIDI_CC12, MIDI_CC13};
+enum midi_cc_keycodes_LTRM {
+	MIDI_CC1 = SAFE_RANGE,
+	MIDI_CC2,
+	MIDI_CC3,
+	MIDI_CC4,
+	MIDI_CC5,
+	MIDI_CC6,
+	MIDI_CC7,
+	MIDI_CC8,
+	MIDI_CC9,
+	MIDI_CC10,
+	MIDI_CC11,
+	MIDI_CC12,
+	MIDI_CC13,
+	MIDI_CC14,
+	MIDI_CC15,
+	MIDI_CC16,
+	MIDI_CC17,
+	MIDI_CC18,
+	MIDI_CC19,
+	MIDI_CC20,
+	MIDI_CC21,
+	MIDI_CC22,
+	MIDI_CC23,
+	MIDI_CC24,
+	MIDI_CC25,
+	L_IND,
+	L_CYC,
+    CULL_MACRO_1,
+    CULL_MACRO_2,
+    CULL_MACRO_3,
+    CULL_MACRO_4,
+    SWITCH_G_L,
+    SHTCT_MACRO_1,
+    SHTCT_MACRO_2,
+    SHTCT_MACRO_3,
+    SHTCT_MACRO_4,
+    SHTCT_MACRO_5,
+    SHTCT_MACRO_6,
+    SHTCT_MACRO_7,
+	SHTCT_MACRO_8,
+};
 
 static char current_alpha_oled = '\0';
 static char current_ltrm_alpha_oled = '\0';
@@ -36,13 +79,65 @@ static char    current_MIDI_ccNumber_char[3] = {'0', '1', '\0'};
 
 // Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_CULL] = LAYOUT(
+        KC_SPACE,                        // Encoder Button
+        CULL_MACRO_1, CULL_MACRO_2, CULL_MACRO_3, CULL_MACRO_4,
+        SWITCH_G_L, KC_UP, KC_S, KC_A,
+        KC_LEFT, KC_DOWN, KC_RGHT, L_CYC
+    ),
     [_LTRM] = LAYOUT(
-        MIDI_CC1,         // Encoder press sends MIDI CC
-        MIDI_CC2, MIDI_CC3, MIDI_CC4, MIDI_CC5,  // Top row keys
-        MIDI_CC6, MIDI_CC7, MIDI_CC8, MIDI_CC9,  // Middle row keys
-        MIDI_CC10, MIDI_CC11, MIDI_CC12, MIDI_CC13  // Bottom row keys
+        MIDI_CC2,                        // Encoder Button
+        MIDI_CC3, MIDI_CC4, MIDI_CC5, MIDI_CC6,
+        MIDI_CC7, MIDI_CC8, MIDI_CC9, MIDI_CC10,
+        MIDI_CC11, MIDI_CC12, MIDI_CC13, L_CYC
+    ),
+    [_PRST] = LAYOUT(
+        KC_SPACE,                        // Encoder Button
+        MIDI_CC15, MIDI_CC16, MIDI_CC17, MIDI_CC18,
+        MIDI_CC19, MIDI_CC20, MIDI_CC21, MIDI_CC22,
+        MIDI_CC23, MIDI_CC24, MIDI_CC25, L_CYC
+    ),
+    [_SHTCT] = LAYOUT(
+        KC_MUTE,                         // Encoder Button
+        SHTCT_MACRO_1, KC_MPLY, KC_MPRV, KC_MNXT,
+        SHTCT_MACRO_2, SHTCT_MACRO_3, SHTCT_MACRO_4, SHTCT_MACRO_5,
+        SHTCT_MACRO_6, SHTCT_MACRO_7, SHTCT_MACRO_8, L_CYC
     ),
 };
+
+/*Custom Keycodes*/
+const rgblight_segment_t PROGMEM layer_zero_all[]  = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_one_all[]   = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_RED});
+const rgblight_segment_t PROGMEM layer_two_all[]   = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_GREEN});
+const rgblight_segment_t PROGMEM layer_three_all[] = RGBLIGHT_LAYER_SEGMENTS({0, 6, HSV_BLUE});
+
+const rgblight_segment_t PROGMEM layer_zero[]  = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_one[]   = RGBLIGHT_LAYER_SEGMENTS({1, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_two[]   = RGBLIGHT_LAYER_SEGMENTS({3, 1, HSV_WHITE});
+const rgblight_segment_t PROGMEM layer_three[] = RGBLIGHT_LAYER_SEGMENTS({4, 1, HSV_WHITE});
+
+const rgblight_segment_t *const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+
+    layer_zero_all,  // 0
+    layer_one_all,   // 1
+    layer_two_all,   // 2
+    layer_three_all, // 3
+    layer_zero,      // 4
+    layer_one,       // 5
+    layer_two,       // 6
+    layer_three      // 7
+
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+#define LAYER_CYCLE_START 0 // 1st layer on the cycle
+#define LAYER_CYCLE_END 3 // Last layer on the cycle
+
+bool led_mode; // false for Blinking Mode, true for Static Mode
 
 // MIDI key handling
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -176,11 +271,316 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
             }
             return false;
-            default:
             break; // Fall through for unhandled keycodes
-    }
+        case MIDI_CC14:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 14;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '1';
+                current_MIDI_ccNumber_char[1] = '4';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC15:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 15;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '1';
+                current_MIDI_ccNumber_char[1] = '5';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC16:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 16;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '1';
+                current_MIDI_ccNumber_char[1] = '6';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC17:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 17;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '1';
+                current_MIDI_ccNumber_char[1] = '7';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC18:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 18;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '1';
+                current_MIDI_ccNumber_char[1] = '8';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC19:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 19;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '1';
+                current_MIDI_ccNumber_char[1] = '9';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC20:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 20;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '2';
+                current_MIDI_ccNumber_char[1] = '0';
+            } else {
+            }
+            return false;
+        case MIDI_CC21:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 21;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '2';
+                current_MIDI_ccNumber_char[1] = '1';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC22:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 22;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '2';
+                current_MIDI_ccNumber_char[1] = '2';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC23:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 23;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '2';
+                current_MIDI_ccNumber_char[1] = '3';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC24:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 24;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '2';
+                current_MIDI_ccNumber_char[1] = '4';
+            } else {
+            }
+            return false;
+            break;
+        case MIDI_CC25:
+            if (record->event.pressed) {
+                current_MIDI_ccNumber         = 25;
+                current_ltrm_alpha_oled       = 'D';
+                current_MIDI_ccNumber_char[0] = '2';
+                current_MIDI_ccNumber_char[1] = '5';
+            } else {
+            }
+            return false;
+            break;
+		case CULL_MACRO_1:
+		    if (record->event.pressed) {
+		        tap_code(KC_1);
+		        tap_code(KC_6);
+		        tap_code(KC_X);
+		        tap_code(KC_RGHT);
+		    }
+		    break;
+		case CULL_MACRO_2:
+		    if (record->event.pressed) {
+		        tap_code(KC_3);
+		        tap_code(KC_7);
+		        tap_code(KC_U);
+		        tap_code(KC_RGHT);
+		    }
+		    break;
+		case CULL_MACRO_3:
+		    if (record->event.pressed) {
+		        tap_code(KC_5);
+		        tap_code(KC_8);
+		        tap_code(KC_P);
+		        tap_code(KC_RGHT);
+		    }
+		    break;
+		case CULL_MACRO_4:
+		    if (record->event.pressed) {
+		        tap_code(KC_5);
+		        tap_code(KC_9);
+		        tap_code(KC_B);
+		        tap_code(KC_P);
+		        tap_code(KC_RGHT);
+		    }
+		    break;
+		case SHTCT_MACRO_1:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_0);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_2:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_1);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_3:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_2);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_4:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_3);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_5:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_4);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_6:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_5);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_7:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_6);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+		case SHTCT_MACRO_8:
+		    if (record->event.pressed) {
+		        register_code(KC_LCTL);
+		        tap_code(KC_7);
+		        unregister_code(KC_LCTL);
+		    }
+		    break;
+        case L_IND:
+            if (record->event.pressed) {
+                // Code to toggle between the two Layer Indicators
+                led_mode = !led_mode; // Toggle the mode
 
-    return true; // Allow further processing for unhandled keycodes
+                // Updateing lighting layer when key is pressed
+                if (led_mode) {
+                    if (layer_state_is(0)) {
+                        rgblight_set_layer_state(4, true);
+                    }
+                    if (layer_state_is(1)) {
+                        rgblight_set_layer_state(5, true);
+                    }
+                    if (layer_state_is(2)) {
+                        rgblight_set_layer_state(6, true);
+                    }
+                    if (layer_state_is(3)) {
+                        rgblight_set_layer_state(7, true);
+                    }
+                } else {
+                    rgblight_set_layer_state(4, false);
+                    rgblight_set_layer_state(5, false);
+                    rgblight_set_layer_state(6, false);
+                    rgblight_set_layer_state(7, false);
+
+                    if (layer_state_is(0)) {
+                        rgblight_blink_layer(0, 1000);
+                    }
+                    if (layer_state_is(1)) {
+                        rgblight_blink_layer(1, 1000);
+                    }
+                    if (layer_state_is(2)) {
+                        rgblight_blink_layer(2, 1000);
+                    }
+                    if (layer_state_is(3)) {
+                        rgblight_blink_layer(3, 1000);
+                    }
+                }
+            } else {
+            }
+            return false; // Skip all further processing of this key
+        case L_CYC:
+            // Our logic will happen on presses, nothing is done on releases
+            if (!record->event.pressed) {
+                // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+                return false;
+            }
+
+            uint8_t current_layer = get_highest_layer(layer_state);
+
+            // Check if we are within the range, if not quit
+            if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
+                return false;
+            }
+
+            uint8_t next_layer = current_layer + 1;
+            if (next_layer > LAYER_CYCLE_END) {
+                next_layer = LAYER_CYCLE_START;
+            }
+            layer_move(next_layer);
+            return false;
+
+        default:
+            return true; // Process all other keycodes normally
+    }
+    return true; // Ensure all paths return a value
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (led_mode) {
+        // Code for Static Layer Indicators
+
+        rgblight_set_layer_state(4, layer_state_cmp(state, 0));
+        rgblight_set_layer_state(5, layer_state_cmp(state, 1));
+        rgblight_set_layer_state(6, layer_state_cmp(state, 2));
+        rgblight_set_layer_state(7, layer_state_cmp(state, 3));
+
+    } else {
+        // Code for Blinking Layer Indicators
+
+        uint8_t layer = get_highest_layer(state);
+
+        switch (layer) {
+            case 0:
+                rgblight_blink_layer(0, 500);
+                break;
+            case 1:
+                rgblight_blink_layer(1, 500);
+                break;
+            case 2:
+                rgblight_blink_layer(2, 500);
+                break;
+            case 3:
+                rgblight_blink_layer(3, 500);
+                break;
+
+            default:
+                rgblight_blink_layer(0, 500);
+        }
+    }
+    return state;
 }
 
 #ifdef OLED_DRIVER_ENABLE
@@ -207,7 +607,7 @@ void oled_task_user(void) {
     }
     switch (get_highest_layer(layer_state)) {
         case _LTRM:
-            oled_write_P(PSTR("3 LTRM"), false);
+            oled_write_P(PSTR("LTRM"), false);
             break;
         // case _MIDI:
         //     oled_write_P(PSTR("4 MIDI"), false);
--- keyboards/themadnoodle/udon13/v2/keymaps/default/rules.mk
+++ keyboards/themadnoodle/udon13/v2/keymaps/default/rules.mk
@@ -5,12 +5,10 @@ EXTRAKEY_ENABLE = yes       # Enable Extra Keys (required for macros)
 CONSOLE_ENABLE = no         # Disable Console (enable temporarily for debugging if needed)
 COMMAND_ENABLE = no         # Disable Command feature
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Disable Backlight
-RGBLIGHT_ENABLE = no       # Enable RGB Lighting (for layer indicators)
 OLED_ENABLE = yes           # Enable OLED Support
 ENCODER_ENABLE = yes        # Enable Rotary Encoder Support
 TAP_DANCE_ENABLE = no       # Not needed for this keymap
-
+RGBLIGHT_ENABLE = yes
 # MIDI Configuration
 MIDI_ENABLE = yes           # Enable MIDI Features
 MIDI_BASIC = yes            # Enable Basic MIDI support
